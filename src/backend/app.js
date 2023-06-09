0edc2e9c4083c57a1a036b23fbc9f7556ce3072d
KristinaGru <grudyte.kristina@gmail.com>
2023-06-09 09:11:28 +0200

    implement feedback

--- src/backend/app.js
+++ src/backend/app.js
@@ -22,36 +22,67 @@ app.use(cors());
 
 router.use('/meals', mealsRouter);
 
-function getMeals(query, status = 200) {
-  return async (req, res) => {
-    try {
-      const meals = await knex.raw(query);
-      if (meals.length > 1 && meals[0].length > 0) {
-        res.status(200).json(meals[0]);
-      } else {
-        if (status === 200) {
-          res.status(200).json('');
-        } else if (status === 404) {
-          res.status(404).json('There are no meals');
-        }
-      }
-    } catch (e) {
-      res.status(500).json(e.message);
+async function getMeals(res, query) {
+  try {
+    const meals = await knex.raw(query);
+    if (meals.length > 1 && meals[0].length > 0) {
+      return meals[0];
+    } else {
+      return [];
     }
-  };
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
 }
 
-app.get(
-  '/future-meals',
-  getMeals('SELECT * FROM meal WHERE `when` > CURDATE()')
-);
-app.get('/past-meals', getMeals('SELECT * FROM meal WHERE `when` < CURDATE()'));
-app.get('/all-meals', getMeals('SELECT * FROM meal ORDER BY id'));
-app.get('/first-meal', getMeals('SELECT * FROM meal ORDER BY id LIMIT 1', 404));
-app.get(
-  '/last-meal',
-  getMeals('SELECT * FROM meal ORDER BY id DESC LIMIT 1', 404)
-);
+function getResponse(res, arr, statusCode = 200) {
+  try {
+    if (statusCode === 404 && arr.length === 0) {
+      res.status(404).json('There are no meals');
+    } else {
+      res.status(statusCode).json(arr);
+    }
+  } catch (e) {
+    res.status(500).json(e.message);
+  }
+}
+
+app.get('/future-meals', async (req, res) => {
+  const meals = await getMeals(
+    res,
+    'SELECT * FROM meal WHERE `when` > CURDATE()'
+  );
+  getResponse(res, meals);
+});
+
+app.get('/past-meals', async (req, res) => {
+  const meals = await getMeals(
+    res,
+    'SELECT * FROM meal WHERE `when` < CURDATE()'
+  );
+  getResponse(res, meals);
+});
+
+app.get('/all-meals', async (req, res) => {
+  const meals = await getMeals(res, 'SELECT * FROM meal ORDER BY id');
+  getResponse(res, meals);
+});
+
+app.get('/first-meal', async (req, res) => {
+  const meal = await getMeals(
+    res,
+    'SELECT * FROM meal ORDER BY id ASC LIMIT 1'
+  );
+  getResponse(res, meal, 404);
+});
+
+app.get('/last-meal', async (req, res) => {
+  const meal = await getMeals(
+    res,
+    'SELECT * FROM meal ORDER BY id DESC LIMIT 1'
+  );
+  getResponse(res, meal, 404);
+});
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
