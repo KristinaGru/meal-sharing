25b57b295afc4cd274f3963a4c09e45e37c6aefc
KristinaGru <grudyte.kristina@gmail.com>
2023-06-13 11:29:16 +0200

    add reservation routes & controller functions

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,77 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../database');
+const {
+  getAllFromTable,
+  addToTable,
+  getById,
+  updateById,
+  deleteById
+} = require('./controller');
 
-router.get('/', async (req, res) => {
-  try {
-    const meals = await knex('meal');
-    res.status(200).json(meals);
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const input = req.body;
-    await knex('meal').insert(input);
-    res.status(201).json({ 'message': `${input.title} added successfuly` });
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    const meal = await knex('meal').where(req.params);
-    if (meal.length > 0) {
-      res.status(200).json(meal);
-    } else {
-      res
-        .status(404)
-        .json({ 'message': `Meal with id ${req.params.id} does not exist` });
-    }
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
-
-router.put('/:id', async (req, res) => {
-  try {
-    const mealId = req.params;
-    const meal = await knex('meal').where(mealId);
-    if (meal.length > 0) {
-      await knex('meal').where(mealId).update(req.body);
-      res
-        .status(201)
-        .json({ 'message': `Meal with id ${mealId.id} updated successfuly` });
-    } else {
-      res
-        .status(404)
-        .json({ 'message': `Meal with id ${mealId.id} does not exist` });
-    }
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const mealId = req.params;
-    const meal = await knex('meal').where(mealId);
-    if (meal.length > 0) {
-      await knex('meal').where(mealId).del();
-      res
-        .status(200)
-        .json({ 'message': `Meal with id ${mealId.id} deleted successfuly` });
-    } else {
-      res
-        .status(404)
-        .json({ 'message': `Meal with id ${mealId.id} does not exist` });
-    }
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-});
+router.get('/', (req, res) => getAllFromTable(req, res, 'meal'));
+router.post('/', (req, res) => addToTable(req, res, 'meal'));
+router.get('/:id', (req, res) => getById(req, res, 'meal'));
+router.put('/:id', (req, res) => updateById(req, res, 'meal'));
+router.delete('/:id', async (req, res) => deleteById(req, res, 'meal'));
 
 module.exports = router;
---
+++ src/backend/api/reservations.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../database');
+const {
+  getAllFromTable,
+  addToTable,
+  getById,
+  updateById,
+  deleteById
+} = require('./controller');
+
+router.get('/', (req, res) => getAllFromTable(req, res, 'reservation'));
+router.post('/', (req, res) => addToTable(req, res, 'reservation'));
+router.get('/:id', (req, res) => getById(req, res, 'reservation'));
+router.put('/:id', (req, res) => updateById(req, res, 'reservation'));
+router.delete('/:id', (req, res) => deleteById(req, res, 'reservation'));
+
+module.exports = router;
