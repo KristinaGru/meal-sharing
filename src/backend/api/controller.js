51918f8bfc21093203b5a12dc84b35a6221de05a
KristinaGru <grudyte.kristina@gmail.com>
2023-06-14 07:58:57 +0200

    implement feedback

--- src/backend/api/controller.js
+++ src/backend/api/controller.js
@@ -21,12 +21,13 @@ const addToTable = async (req, res, table) => {
 
 const getById = async (req, res, table) => {
   try {
-    const data = await knex(table).where(req.params);
+    const { id } = req.params;
+    const data = await knex(table).where({ id });
     if (data.length > 0) {
       res.status(200).json(data);
     } else {
       res.status(404).json({
-        'message': `${table} with id ${req.params.id} does not exist`
+        'message': `${table} with id ${{ id }.id} does not exist`
       });
     }
   } catch (e) {
@@ -36,17 +37,17 @@ const getById = async (req, res, table) => {
 
 const updateById = async (req, res, table) => {
   try {
-    const id = req.params;
-    const data = await knex(table).where(id);
+    const { id } = req.params;
+    const data = await knex(table).where({ id });
     if (data.length > 0) {
-      await knex(table).where(id).update(req.body);
-      res
-        .status(201)
-        .json({ 'message': `${table} with id ${id.id} updated successfuly` });
+      await knex(table).where({ id }).update(req.body);
+      res.status(201).json({
+        'message': `${table} with id ${{ id }.id} updated successfuly`
+      });
     } else {
-      res
-        .status(404)
-        .json({ 'message': `${table} with id ${id.id} does not exist` });
+      res.status(404).json({
+        'message': `${table} with id ${{ id }.id} does not exist`
+      });
     }
   } catch (e) {
     res.status(500).json(e.message);
@@ -55,17 +56,17 @@ const updateById = async (req, res, table) => {
 
 const deleteById = async (req, res, table) => {
   try {
-    const id = req.params;
-    const data = await knex(table).where(id);
+    const { id } = req.params;
+    const data = await knex(table).where({ id });
     if (data.length > 0) {
-      await knex(table).where(id).del();
-      res
-        .status(200)
-        .json({ 'message': `${table} with id ${id.id} deleted successfuly` });
+      await knex(table).where({ id }).del();
+      res.status(200).json({
+        'message': `${table} with id ${{ id }.id} deleted successfuly`
+      });
     } else {
       res
         .status(404)
-        .json({ 'message': `${table} with id ${id.id} does not exist` });
+        .json({ 'message': `${table} with id ${{ id }.id} does not exist` });
     }
   } catch (e) {
     res.status(500).json(e.message);
