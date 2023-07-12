8b4cace7e5564627ddc9a317652b8e8fea53a228
KristinaGru <grudyte.kristina@gmail.com>
2023-07-12 19:21:00 +0200

    add MealsList and Meal components

---
+++ src/client/components/TestComponent/MealsList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Meal from './Meal';
+
+const MealsList = () => {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    async function fetchMeals() {
+      const res = await fetch('http://localhost:3000/api/meals');
+      const meals = await res.json();
+      setMeals(meals);
+    }
+    fetchMeals().catch(console.error);
+  }, []);
+
+  return (
+    <ul>
+      {meals.map((meal) => (
+        <Meal meal={meal} />
+      ))}
+    </ul>
+  );
+};
+
+export default MealsList;
